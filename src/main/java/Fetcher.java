cecae37f06700179f69f6a10138cef3635694c69
Cornelius Davis <valentacodes@gmail.com>
2025-12-22 19:34:15 -0700

    Dev (#7)
    * Processing Headers WIP
    
    * Might use stack to store sequential redirects
    
    * Objectifying project
    
    * request/response/stack
    
    * Cleaned up Fetcher
    
    * Might use stack to store sequential redirects

--- src/main/java/Fetcher.java
+++ src/main/java/Fetcher.java
@@ -13,6 +13,7 @@ public class Fetcher {
     private static final Set<String> tldSet = Set.of("com", "org", "net", "edu", "gov", "io", "app", "dev", "ai", "xyz",
             "us", "uk", "co", "me", "tv", "info", "biz");
     private final HashMap<String, List<String>> headerMap;
+    private int redirects = 0;
 //    private static final String TLD_RESOURCE_PATH = "/tlds.txt";
 
     public Fetcher() {
